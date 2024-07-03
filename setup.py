c17d9bd26794977dc0d27c8f1f5c5a9e8a5fb6dd
BocciGaria <s.otsuka0726@gmail.com>
2024-07-03 19:50:57 +0900

    init package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_deep_learning",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+    ],
+)
