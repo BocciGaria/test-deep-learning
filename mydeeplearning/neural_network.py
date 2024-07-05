a94cab001687c95fd12f8fcb0d10d91197d92df5
BocciGaria <s.otsuka0726@gmail.com>
2024-07-05 16:07:42 +0900

    Implemented forwarding function of the multi-layered neural network

--- mydeeplearning/neural_network.py
+++ mydeeplearning/neural_network.py
@@ -1,29 +1,56 @@
 import numpy
 
+from mydeeplearning.activation import identity_function
+
 
 class Layer:
     """ネットワークの層"""
-    __nl: "Layer" = None
+    _nl: "Layer" = None
 
     def __init__(self, weight, bias, activation) -> None:
-        self.__w = numpy.array(weight)
-        self.__b = numpy.array(bias)
-        self.__h = activation
+        self._w = numpy.array(weight)
+        self._b = numpy.array(bias)
+        self._h = activation
 
     def forward(self, x) -> numpy.ndarray:
-        a = numpy.dot(x, self.__w) + self.__b
-        z = self.__h(a)
-        if self.__nl is not None:
-            return self.__nl.forward(z)
+        a = numpy.dot(x, self._w) + self._b
+        z = self._h(a)
+        if self._nl is not None:
+            return self._nl.forward(z)
         else:
             return z
 
-    def set_next_layer(self, l) -> None:
-        self.__nl = l
+    def forwarding_to(self, l) -> None:
+        self._nl = l
+
+    @property
+    def remaining_forwarding_len(self) -> int:
+        if self._nl is None:
+            return 1
+        else:
+            return self._nl.remaining_forwarding_len + 1
 
 
-class NeuralNetwork:
+class NeuralNetwork(Layer):
     """ニューラルネットワーク"""
 
+    def __init__(self, fianl_activation=identity_function) -> None:
+        super().__init__(1, 0, identity_function)
+        self._fa = fianl_activation
+
     def forward(self, x) -> numpy.ndarray:
-        return numpy.array(x)
+        return self._fa(super().forward(x))
+
+    def add(self, *layers: Layer) -> None:
+        if self._nl is None:
+            self.forwarding_to(layers[0])
+        else:
+            self._nl.forwarding_to(layers[0])
+        previous = layers[0]
+        for next in layers[1:]:
+            previous.forwarding_to(next)
+            previous = next
+
+    @property
+    def dim(self) -> int:
+        return self.remaining_forwarding_len - 1
--- tests/mydeeplearning/test_neural_network.py
+++ tests/mydeeplearning/test_neural_network.py
@@ -1,7 +1,23 @@
 import numpy
+import pytest
 
 from mydeeplearning.activation import identity_function, sigmoid, softmax
 from mydeeplearning.neural_network import NeuralNetwork, Layer
+
+
+@pytest.fixture
+def layers():
+    weight1 = [[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]
+    weight2 = [[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]
+    weight3 = [[0.1, 0.3], [0.2, 0.4]]
+    bias1 = [0.1, 0.2, 0.3]
+    bias2 = [0.1, 0.2]
+    bias3 = [0.1, 0.2]
+    return (
+        Layer(weight1, bias1, sigmoid),
+        Layer(weight2, bias2, sigmoid),
+        Layer(weight3, bias3, identity_function)
+    )
 
 
 def test_generate_network():
@@ -22,40 +38,27 @@ def test_forward_layer():
     assert numpy.allclose(y, [0.76852478, 0.92414182, 0.97811873])
 
 
-def test_forward_multiple_layers():
-    weight1 = [[0.1, 0.3, 0.5], [0.2, 0.4, 0.6]]
-    weight2 = [[0.1, 0.4], [0.2, 0.5], [0.3, 0.6]]
-    weight3 = [[0.1, 0.3], [0.2, 0.4]]
-    bias1 = [0.1, 0.2, 0.3]
-    bias2 = [0.1, 0.2]
-    bias3 = [0.1, 0.2]
-    l1 = Layer(weight1, bias1, sigmoid)
-    l2 = Layer(weight2, bias2, sigmoid)
-    l3 = Layer(weight3, bias3, identity_function)
-    l1.set_next_layer(l2)
-    l2.set_next_layer(l3)
-    y = l1.forward([1.0, 0.5])
+def test_forward_multiple_layers(layers):
+    layers[0].forwarding_to(layers[1])
+    layers[1].forwarding_to(layers[2])
+    y = layers[0].forward([1.0, 0.5])
     assert numpy.allclose(y, [0.31682708, 0.69627909])
 
 
-def test_add_hidden_layer():
-    # ネットワークを生成する
-    # 隠れ層を生成する（重み、バイアス、活性化関数）
-    # 隠れ層をネットワークに挿入する
-    # 何を検証する？
-    pass
+def test_get_remaining_forwarding_length(layers):
+    layers[0].forwarding_to(layers[1])
+    layers[1].forwarding_to(layers[2])
+    assert layers[0].remaining_forwarding_len == 3
 
 
-def test_forward_single_layered_neural_network():
-    # ネットワークを生成する 初期値:[1.0, 0.5]
-    # 隠れ層を生成する
-    # 隠れ層を追加する
-    # 入力から出力への伝達処理を行う
-    # assert numpy.allclose(y, numpy.array([0.31682708, 0.69627909]))
-    pass
-
-
-def test_forward_neural_network():
+def test_add_hidden_layer(layers):
     net = NeuralNetwork()
+    net.add(*layers)
+    assert net.dim == 3
+
+
+def test_forward_neural_network(layers):
+    net = NeuralNetwork(softmax)
+    net.add(*layers)
     y = net.forward([1.0, 0.5])
-    assert numpy.array_equal(y, [1.0, 0.5])
+    assert numpy.allclose(y, [0.40625907, 0.59374093])
