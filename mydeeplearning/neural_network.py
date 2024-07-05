e72f131d63476e2f73fe1e93e4988a97d4e880f5
BocciGaria <s.otsuka0726@gmail.com>
2024-07-05 17:29:51 +0900

    Added comments

--- mydeeplearning/neural_network.py
+++ mydeeplearning/neural_network.py
@@ -1,3 +1,11 @@
+"""ニューラルネットワーク
+
+作成日   : 2024/07/05
+作成者   : BocciGaria
+最終更新日: 2024/07/05
+最終更新者: BocciGaria
+"""
+
 import numpy
 
 from mydeeplearning.activation import identity_function
@@ -8,11 +16,35 @@ class Layer:
     _nl: "Layer" = None
 
     def __init__(self, weight, bias, activation) -> None:
+        """
+        ニューラルネットワークを構成する層を生成する
+
+        Parameters
+        ----------
+        weight : array
+            この層への入力数に応じた重み（入力と出力がともに１つでない限り配列）
+        bias : array
+            この層の出力数に応じたバイアス（出力が１つでない限り配列）
+        activation : function
+            この層のニューロンで実行する活性化関数
+        """
         self._w = numpy.array(weight)
         self._b = numpy.array(bias)
         self._h = activation
 
     def forward(self, x) -> numpy.ndarray:
+        """入力から出力への伝達処理
+
+        Parameters
+        ----------
+        x : array
+            入力値（入力が１つでない限り配列）
+
+        Returns
+        -------
+        numpy.ndarray
+            出力値（ニューロンが１つでない限り配列）
+        """
         a = numpy.dot(x, self._w) + self._b
         z = self._h(a)
         if self._nl is not None:
@@ -20,11 +52,13 @@ class Layer:
         else:
             return z
 
-    def forwarding_to(self, l) -> None:
-        self._nl = l
+    def forwarding_to(self, layer) -> None:
+        """フォワード時の伝達先層を設定する"""
+        self._nl = layer
 
     @property
     def remaining_forwarding_len(self) -> int:
+        """フォワード時の残りの伝達回数（出力方向にある層の数に等しい）"""
         if self._nl is None:
             return 1
         else:
@@ -32,6 +66,7 @@ class Layer:
 
     @property
     def last_layer(self) -> "Layer":
+        """フォワード時の最終伝達先層"""
         if self._nl is None:
             return self
         else:
@@ -42,6 +77,14 @@ class NeuralNetwork(Layer):
     """ニューラルネットワーク"""
 
     def __init__(self, fianl_activation=identity_function) -> None:
+        """
+        ニューラルネットワークを生成する
+
+        Parameters
+        ----------
+        final_activation : function
+            ネットワークの出力層のニューロンで実行される活性化関数
+        """
         super().__init__(1, 0, identity_function)
         self._fa = fianl_activation
 
@@ -49,6 +92,14 @@ class NeuralNetwork(Layer):
         return self._fa(super().forward(x))
 
     def add(self, *layers: Layer) -> None:
+        """隠し層を追加する
+        追加した順に接続され、新しい層は出力方向の最後の層の後ろに接続される
+
+        Parameters
+        ----------
+        *layers : tuple[Layer]
+            追加する隠し層（複数追加する場合は引数に指定した順に追加される）
+        """
         if self._nl is None:
             self.forwarding_to(layers[0])
         else:
@@ -60,4 +111,7 @@ class NeuralNetwork(Layer):
 
     @property
     def dim(self) -> int:
+        """ネットワークの次元数
+        ３層ネットワークであれば３が得られる
+        """
         return self.remaining_forwarding_len - 1
