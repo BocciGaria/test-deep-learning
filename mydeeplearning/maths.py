dfb5d3ddf773d3a45bd1ee95cca409493f80528f
BocciGaria <s.otsuka0726@gmail.com>
2024-07-08 14:23:02 +0900

    Implemented the numerical gradient function

--- mydeeplearning/maths.py
+++ mydeeplearning/maths.py
@@ -6,8 +6,26 @@
 最終更新者: BocciGaria
 """
 
+import numpy
+
 
 def numerical_diff(f, x):
     """数値微分"""
     h = 1e-4
     return (f(x + h) - f(x - h)) / (2 * h)
+
+
+def numerical_gradient(f, x):
+    """勾配"""
+    result = numpy.zeros_like(x)
+    h = 1e-4
+    nda_x = numpy.array(x)
+    for idx in range(nda_x.size):
+        tmp_upper_x = nda_x.copy()
+        tmp_upper_x[idx] = tmp_upper_x[idx] + h
+        y_upper = f(tmp_upper_x)
+        tmp_lower_x = nda_x.copy()
+        tmp_lower_x[idx] = tmp_lower_x[idx] - h
+        y_lower = f(tmp_lower_x)
+        result[idx] = (y_upper - y_lower) / (h * 2)
+    return result
--- tests/mydeeplearning/test_maths.py
+++ tests/mydeeplearning/test_maths.py
@@ -1,9 +1,20 @@
 from decimal import Decimal
+import numpy
 
-from mydeeplearning.maths import numerical_diff
+from mydeeplearning.maths import numerical_diff, numerical_gradient
 
 
 def test_numerical_diff():
     f = lambda x: 0.01 * x**2 + 0.1 * x
     assert Decimal("0.2000") == Decimal(numerical_diff(f, 5)).quantize(Decimal("1e-4"))
     assert Decimal("0.3000") == Decimal(numerical_diff(f, 10)).quantize(Decimal("1e-4"))
+
+
+def test_numerical_gradient():
+    f = lambda x: numpy.sum(x[0]**2 + x[1]**2)
+    gradient = numerical_gradient(f, [3.0, 4.0])
+    assert numpy.allclose([6.0, 8.0], gradient)
+    gradient = numerical_gradient(f, [0.0, 2.0])
+    assert numpy.allclose([0.0, 4.0], gradient)
+    gradient = numerical_gradient(f, [3.0, 0.0])
+    assert numpy.allclose([6.0, 0.0], gradient)
