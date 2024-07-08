51e380d5209a5d26e333a71f3dd9f8557b493512
BocciGaria <s.otsuka0726@gmail.com>
2024-07-08 14:51:17 +0900

    Add comments

--- mydeeplearning/maths.py
+++ mydeeplearning/maths.py
@@ -10,13 +10,41 @@ import numpy
 
 
 def numerical_diff(f, x):
-    """数値微分"""
+    """数値微分
+    ひとつのパラメータを持つ関数fのx地点における数値微分を取得する
+
+    Parameters
+    ----------
+    f : function
+        ひとつのパラメータを持つ関数
+    x : array
+        関数fへのパラメータ
+
+    Returns
+    -------
+    float
+        fにおけるx地点の数値微分
+    """
     h = 1e-4
     return (f(x + h) - f(x - h)) / (2 * h)
 
 
 def numerical_gradient(f, x):
-    """勾配"""
+    """勾配
+    複数のパラメータを持つ関数fのx地点における偏微分を取得する
+
+    Parameters
+    ----------
+    f : function
+        複数のパラメータを持つ関数
+    x : array
+        関数fへのパラメータ群
+
+    Returns
+    -------
+    array[float]
+        fにおけるx地点の勾配
+    """
     result = numpy.zeros_like(x)
     h = 1e-4
     nda_x = numpy.array(x)
