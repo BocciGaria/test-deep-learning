d774065df972793be7ae9e43da9ad67be667111c
BocciGaria <s.otsuka0726@gmail.com>
2024-07-12 22:35:47 +0900

    Added a test to combine the mulplication node and addition node.

--- tests/mydeeplearning/test_computational_graph.py
+++ tests/mydeeplearning/test_computational_graph.py
@@ -37,3 +37,33 @@ def test_add_node_backward():
     dx, dy = n.backward(1.1)
     assert math.isclose(1.1, dx)
     assert math.isclose(1.1, dy)
+
+
+def test_combine_mul_node_add_node():
+    apple_unit_price = 100.0
+    apple_num = 2.0
+    orange_unit_price = 150.0
+    orange_num = 3.0
+    tax = 1.1
+
+    apple_node = MulNode()
+    orange_node = MulNode()
+    add_node = AddNode()
+    tax_node = MulNode()
+
+    apple_price = apple_node.forward(apple_unit_price, apple_num)
+    orange_price = orange_node.forward(orange_unit_price, orange_num)
+    total_price = add_node.forward(apple_price, orange_price)
+    total_price_with_tax = tax_node.forward(total_price, tax)
+
+    d_total_price, d_tax = tax_node.backward(1)
+    d_apple_price, d_orange_price = add_node.backward(d_total_price)
+    d_orange_unit_price, d_orange_num = orange_node.backward(d_orange_price)
+    d_apple_unit_price, d_apple_num = apple_node.backward(d_apple_price)
+
+    assert math.isclose(715.0, total_price_with_tax)
+    assert math.isclose(110.0, d_apple_num)
+    assert math.isclose(2.2, d_apple_unit_price)
+    assert math.isclose(165.0, d_orange_num)
+    assert math.isclose(3.3, d_orange_unit_price)
+    assert math.isclose(650.0, d_tax)
