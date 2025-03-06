e14abbdfe0aa8beb84147a97d1cb2824c34930eb
BocciGaria <s.otsuka0726@gmail.com>
2025-03-06 13:46:38 +0900

    refactoring

--- mydeeplearning/perceptron.py
+++ mydeeplearning/perceptron.py
@@ -1,15 +1,15 @@
-import numpy
+import numpy as np
 
 
-def logic_gate(x: numpy.ndarray, w, b):
+def _logic_gate(x: np.ndarray, w, b):
     """論理ゲート"""
     if x.ndim > 1:
         raise ValueError("入力値の次元数が多すぎます。x.ndim: %s" % x.ndim)
-    if not numpy.isin(x, (0, 1, True, False)).all():
+    if not np.isin(x, (0, 1, True, False)).all():
         raise ValueError(
             "入力値は[1, 0, True, False]のいずれかでなくてはなりません。x: %s" % x
         )
-    y = numpy.sum(x * w) + b
+    y = np.sum(x * w) + b
     if y <= 0:
         return 0
     else:
@@ -18,28 +18,28 @@ def logic_gate(x: numpy.ndarray, w, b):
 
 def AND(x1, x2):
     """ANDゲート"""
-    x = numpy.array([x1, x2])
-    w = numpy.array([0.5, 0.5])
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
     b = -0.7
-    return logic_gate(x, w, b)
+    return _logic_gate(x, w, b)
 
 
 def OR(x1, x2):
     """ORゲート"""
-    x = numpy.array([x1, x2])
-    w = numpy.array([0.5, 0.5])
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
     b = -0.2
-    y = numpy.sum(x * w) + b
-    return logic_gate(x, w, b)
+    y = np.sum(x * w) + b
+    return _logic_gate(x, w, b)
 
 
 def NAND(x1, x2):
     """NANDゲート"""
-    x = numpy.array([x1, x2])
-    w = numpy.array([-0.5, -0.5])
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
     b = 0.7
-    y = numpy.sum(x * w) + b
-    return logic_gate(x, w, b)
+    y = np.sum(x * w) + b
+    return _logic_gate(x, w, b)
 
 
 def XOR(x1, x2):
