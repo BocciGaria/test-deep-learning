92aceeb13216ebdbc5552e897e91c38f477b23a6
BocciGaria <s.otsuka0726@gmail.com>
2025-03-12 16:10:26 +0900

    Implemented TwoLayerNet

--- mydeeplearning/neuralnet.py
+++ mydeeplearning/neuralnet.py
@@ -1,6 +1,8 @@
 import numpy as np
 
-from mydeeplearning.activation_func import identity_function, sigmoid
+from mydeeplearning.activation_func import identity_function, sigmoid, softmax
+from mydeeplearning.loss_func import cross_entropy_error
+from mydeeplearning.maths import numerical_gradient
 
 
 def init_network():
@@ -29,3 +31,144 @@ def forward(network, x):
     y = identity_function(a3)
 
     return y
+
+
+class INeuralNet:
+    def predict(self, x) -> np.ndarray:
+        """予測
+
+        ニューラルネットワークへの入力に対する予測結果を得る。
+
+        Parameters
+        ----------
+        x : ArrayLike
+            入力の配列
+
+        Returns
+        -------
+        : numpy.ndarray
+            予測結果の配列
+        """
+        raise NotImplementedError()
+
+    def loss(self, x, t) -> np.floating:
+        """損失
+
+        ニューラルネットワークへの入力に対する予測結果と教師データによって呼び出し時点の損失を求める。
+
+        Parameters
+        ----------
+        x : ArrayLike
+            入力の配列
+        t : ArrayLike
+            教師データ
+
+        Returns
+        -------
+        : float
+            損失
+        """
+        raise NotImplementedError()
+
+
+class SimpleNet(INeuralNet):
+    """単純なニューラルネットワーク"""
+
+    def __init__(self):
+        self.weights = np.random.randn(2, 3)  # ガウス分布で初期化
+
+    def predict(self, x):
+        return np.dot(x, self.weights)
+
+    def loss(self, x, t):
+        z = self.predict(x)
+        y = softmax(z)
+        return cross_entropy_error(y, t)
+
+
+class TwoLayerNet(INeuralNet):
+    """２層ニューラルネットワーク"""
+
+    def __init__(self, input_size, hidden_size, output_size, weight_init_std=0.01):
+        """コンストラクタ
+
+        Parameters
+        ----------
+        input_size : int
+            入力層のニューロンの数
+        hidden_size : int
+            隠れ層のニューロンの数
+        output_size : int
+            出力層のニューロンの数
+        weight_init_std : float
+            重みの初期値の標準偏差
+        """
+        # 重みの初期化
+        self.params = {}
+        self.params["W1"] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params["b1"] = np.zeros(hidden_size)
+        self.params["W2"] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params["b2"] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params["W1"], self.params["W2"]
+        b1, b2 = self.params["b1"], self.params["b2"]
+
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+
+        return softmax(a2)
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        """認識精度を求める
+
+        Parameters
+        ----------
+        x : ArrayLike
+            入力値の配列
+        t : ArrayLike
+            教師データの配列
+
+        Returns
+        -------
+        : np.floating
+            認識精度(0.0 ~ 1.0)
+        """
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        return np.sum(y == t) / float(x.shape[0])
+
+    def numerical_gradient(self, x, t):
+        """重みパラメータに対する勾配を数値微分によって求める
+
+        Parameters
+        ----------
+        x : ArrayLike
+            入力値の配列
+        t : ArrayLike
+            教師データの配列
+
+        Returns
+        -------
+        : dict[np.ndarray]
+            重みパラメータに対する勾配
+        """
+        # Wの参照渡しによって成立している->膨大なパラメータをコピーしなくていいことのトレードオフとして副作用あり
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {
+            "W1": numerical_gradient(loss_W, self.params["W1"]),
+            "b1": numerical_gradient(loss_W, self.params["b1"]),
+            "W2": numerical_gradient(loss_W, self.params["W2"]),
+            "b2": numerical_gradient(loss_W, self.params["b2"]),
+        }
+
+        return grads
--- tests/mydeeplearning/test_neuralnet.py
+++ tests/mydeeplearning/test_neuralnet.py
@@ -1,11 +1,95 @@
 import numpy as np
+import pytest
 
-from mydeeplearning import neuralnet
+from mydeeplearning.neuralnet import forward, init_network, SimpleNet, TwoLayerNet
 
 
 def test_forward_network():
     x = [1.0, 0.5]
-    y = neuralnet.forward(neuralnet.init_network(), x)
+    y = forward(init_network(), x)
     assert np.allclose(y, np.array([0.31682708, 0.69627909]), rtol=1e-8, atol=1e-8)
     # assert not np.allclose(y, np.array([0.31682713, 0.69627914]), rtol=1e-8, atol=1e-8)
     # assert not np.allclose(y, np.array([0.31682703, 0.69627904]), rtol=1e-8, atol=1e-8)
+
+
+@pytest.fixture
+def simple_net():
+    return SimpleNet()
+
+
+def test_simplenet_instance(simple_net):
+    assert simple_net.weights.shape == (2, 3)
+    assert simple_net.weights.dtype == np.floating
+
+
+def test_simplenet_prediction(simple_net):
+    prediction = simple_net.predict([0, 0])
+    assert prediction.shape == (3,)
+    assert prediction.dtype == np.floating
+
+
+def test_simplenet_loss(simple_net):
+    loss = simple_net.loss([0, 0], [0, 0, 1])
+    assert loss.ndim == 0
+    assert loss.dtype == np.floating
+
+
+@pytest.fixture
+def two_layer_net_mnist():
+    return TwoLayerNet(784, 100, 10)
+
+
+@pytest.fixture
+def input_mnist():
+    return np.random.rand(100, 784)
+
+
+@pytest.fixture
+def teacher_mnist():
+    return np.random.rand(100, 10)
+
+
+def test_twolayernet_instance(two_layer_net_mnist):
+    assert two_layer_net_mnist.params["W1"].shape == (784, 100)
+    assert two_layer_net_mnist.params["b1"].shape == (100,)
+    assert two_layer_net_mnist.params["W2"].shape == (100, 10)
+    assert two_layer_net_mnist.params["b2"].shape == (10,)
+
+
+def test_twolayernet_prediction(two_layer_net_mnist, input_mnist):
+    y = two_layer_net_mnist.predict(input_mnist)
+    assert y.shape == (100, 10)
+    assert y.dtype == np.floating
+    assert np.all([y >= 0] and [y <= 1])
+
+
+def test_twolayernet_loss(two_layer_net_mnist, input_mnist, teacher_mnist):
+    loss = two_layer_net_mnist.loss(input_mnist, teacher_mnist)
+    assert loss.ndim == 0
+    assert loss.dtype == np.floating
+
+
+def test_twolayernet_accuracy(two_layer_net_mnist, input_mnist, teacher_mnist):
+    a = two_layer_net_mnist.accuracy(input_mnist, teacher_mnist)
+    assert a.ndim == 0
+    assert a.dtype == np.floating
+    assert a >= 0 and a <= 1
+
+
+def test_twolayernet_numerical_gradient(
+    two_layer_net_mnist, input_mnist, teacher_mnist
+):
+    # grads = two_layer_net_mnist.numerical_gradient(input_mnist, teacher_mnist)
+    # assert grads["W1"].shape == (784, 100)
+    # assert grads["b1"].shape == (100,)
+    # assert grads["W2"].shape == (100, 10)
+    # assert grads["b2"].shape == (10,)
+    # 処理が遅いのでデータ数を減らす
+    net = TwoLayerNet(64, 15, 10)
+    x = np.random.rand(5, 64)
+    t = np.random.rand(5, 10)
+    grads = net.numerical_gradient(x, t)
+    assert grads["W1"].shape == (64, 15)
+    assert grads["b1"].shape == (15,)
+    assert grads["W2"].shape == (15, 10)
+    assert grads["b2"].shape == (10,)
